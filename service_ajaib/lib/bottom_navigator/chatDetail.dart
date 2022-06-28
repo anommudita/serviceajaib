1bf6d52b73e6aef5a131fb69600ec3ec27526d45
anommudita <gusmangananada@gmail.com>
2022-06-28 09:50:47 +0800

    upload file detail chat main

---
+++ service_ajaib/lib/bottom_navigator/chatDetail.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import '../models/chatMessageModel.dart';
+
+class ChatDetailPage extends StatefulWidget {
+  @override
+  _ChatDetailPageState createState() => _ChatDetailPageState();
+}
+
+class _ChatDetailPageState extends State<ChatDetailPage> {
+  List<ChatMessage> messages = [
+    ChatMessage(messageContent: "Hello, Kerta", messageType: "receiver"),
+    ChatMessage(
+        messageContent: "Apakah anda memesan teknisi?",
+        messageType: "receiver"),
+    ChatMessage(
+        messageContent: "Hey Anom, Iya saya memesannya", messageType: "sender"),
+    ChatMessage(messageContent: "Woke OTW", messageType: "receiver"),
+    ChatMessage(messageContent: "Siappp", messageType: "sender"),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('Kerta')),
+        body: ListView.builder(
+          itemCount: messages.length,
+          shrinkWrap: true,
+          padding: EdgeInsets.only(top: 10, bottom: 10),
+          physics: NeverScrollableScrollPhysics(),
+          itemBuilder: (context, index) {
+            return Container(
+              padding:
+                  EdgeInsets.only(left: 14, right: 14, top: 10, bottom: 10),
+              child: Align(
+                alignment: (messages[index].messageType == "receiver"
+                    ? Alignment.topLeft
+                    : Alignment.topRight),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: (messages[index].messageType == "receiver"
+                        ? Colors.grey.shade200
+                        : Colors.blue[200]),
+                  ),
+                  padding: EdgeInsets.all(16),
+                  child: Text(
+                    messages[index].messageContent,
+                    style: TextStyle(fontSize: 15),
+                  ),
+                ),
+              ),
+            );
+          },
+        ));
+  }
+}
---
+++ service_ajaib/lib/bottom_navigator/diterimaPage.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class Diterima extends StatefulWidget {
+  Diterima({Key? key}) : super(key: key);
+  @override
+  State<Diterima> createState() => _Diterima();
+}
+
+class _Diterima extends State<Diterima> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          title: Text('SERVICE AJAIB',
+              style: TextStyle(
+                  color: Color.fromARGB(255, 209, 209, 209),
+                  fontSize: 14,
+                  fontFamily: 'Roboto')),
+        ),
+        body: ListView(
+          children: [
+            Container(
+              // ignore: unnecessary_new
+              child: new Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  // ignore: unnecessary_new
+                  new Container(
+                    child: const Icon(
+                      Icons.check_circle_outline,
+                      color: Colors.blue,
+                      size: 24.0,
+                      semanticLabel: 'Pesananmu sudah kami terima',
+                    ),
+                  ),
+                  new Container(
+                    width: 200,
+                    height: 45,
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                        backgroundColor: Colors.blue,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        "HOME",
+                        style: TextStyle(
+                          color: Color(0xffffffff),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ));
+  }
+}
